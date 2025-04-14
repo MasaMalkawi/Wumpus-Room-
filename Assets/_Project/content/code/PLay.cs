9ef47df6d1bfced3551d08761e6b1b61d6c8407b
MasaMalkawi <masamalkawi@gmail.com>
2025-04-14 11:04:57 +0300

    floor update

--- Assets/_Project/content/code/PLay.cs
+++ Assets/_Project/content/code/PLay.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,5 +13,27 @@ public class PLay : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    [UnityEditor.MenuItem("Masa/Fix Tiles")]
+    static void Fix()
+    {
+        GameObject[] allObjects = Object.FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID);
+
+        List<GameObject> allTiles = allObjects.Where(o => o.name.Contains("Floor_01") && o.GetComponent<Renderer>() != null).ToList();
+        Debug.Log(allTiles.Count);
+
+        foreach (GameObject tile in allTiles)
+        {
+            UnityEditor.Undo.RecordObject(tile.transform, "Fix Tiles");
+            Vector3 pos = tile.transform.position;
+
+            pos.x = (int)(pos.x / 3f) * 3;
+            pos.z = (int)(pos.z / 3f) * 3;
+
+            tile.transform.position = pos;
+        }
+
+        Debug.Log("Tiles fixed!");
     }
 }
